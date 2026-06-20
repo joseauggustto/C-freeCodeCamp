ae45fb24b01e936dfc1bc7f61224bf84e0733793
joseauggustto <jose.morais22aluno@gmail.com>
2026-06-20 10:56:08 -0300

    First Codes

---
+++ Primeiros Códigos.cs
@@ -0,0 +1,20 @@
+// Console.WriteLine("Hello World!");
+
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.WriteLine("You wrote your first lines of code.");
+
+// ---------
+Console.WriteLine(" "); // Pular uma simples linha para organizar o código.
+// ---------
+
+Console.WriteLine("Congratulations!");
+Console.Write("You wrote your first lines of code.");   // Esse comando escreve a mensagem mas sem pular para a próxima linha. 
+
+// --------
+
+// Desafio 1
+
+Console.WriteLine("This is the first line.");
+Console.Write("This is the second line.");
+
