819ba58772e041d6331dccedfcc7778b3754a41e
joseauggustto <jose.morais22aluno@gmail.com>
2026-06-21 15:04:35 -0300

    Ok

---
+++ Formatação de Cadeias.cs
@@ -0,0 +1,7 @@
+Console.WriteLine("Hello\nWorld!"); // Adiciona uma nova linha.
+Console.WriteLine("Hello\tWorld!"); // Adiciona um tab.
+
+Console.WriteLine("Hello \"World\"!"); // Adiciona aspas duplas.
+
+// --------
+
